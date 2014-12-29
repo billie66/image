430b8e0fc04148caf39796ed4052252161b7cabd
Billie Zhang <billiecoder@gmail.com>
2014-12-29 22:47:19 +0800

    upload images

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,14 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+  def new
+    @course = Course.new
+  end
+  def show
+    @course = Course.find(params[:id])
+  end
+  def create
+
+  end
+end
