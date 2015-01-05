99445eccd0ebad045688c886326e4a7fc5abaacc
Billie Zhang <billiecoder@gmail.com>
2015-01-05 14:17:15 +0800

    adjust image size

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ActiveRecord::Base
   mount_uploader :cover, CoverUploader
+  before_create :cover_name
+
+  def cover_name
+      self.name ||= File.basename(cover.filename, '.*').titleize if cover
+  end
 end
