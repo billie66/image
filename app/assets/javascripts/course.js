065a318620f2306cf3465eff12e00888186e7613
Billie Zhang <billiecoder@gmail.com>
2015-01-06 11:47:34 +0800

    validate file type on client end

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -1,5 +1,15 @@
 $(function() {
   return $('.new-course').fileupload({
-    dataType: "script"
+    dataType: "script",
+    add: function(e, data) {
+      var file, types;
+      types = /(\.|\/)(gif|jpe?g|png)$/i;
+      file = data.files[0];
+      if (types.test(file.type) || types.test(file.name)) {
+        return data.submit();
+      } else {
+        return alert("" + file.name + " is not a gif, jpeg, or png image file");
+      }
+    }
   });
 });
