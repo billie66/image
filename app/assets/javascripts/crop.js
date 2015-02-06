f0d4d72b6945e324f23e70ecd2e25f2db69c7cdf
Billie Zhang <billiecoder@gmail.com>
2015-02-06 21:50:35 +0800

    refactor code

--- app/assets/javascripts/crop.js
+++ app/assets/javascripts/crop.js
@@ -1,8 +1,27 @@
 $(function() {
   var jcrop_api; // Holder for the jcrop API
   var inputFile = $("#fileupload")[0];
-  inputFile.addEventListener('click', function() { this.value = null; }, false);
-  inputFile.addEventListener('change', readData, false);
+
+  function update_crop(coords) {
+    $("#user_crop_x").val(Math.round(coords.x));
+    $("#user_crop_y").val(Math.round(coords.y));
+    $("#user_crop_w").val(Math.round(coords.w));
+    $("#user_crop_h").val(Math.round(coords.h));
+  }
+
+  function init_jcrop() {
+    $(document).bind('cbox_complete', function() {
+      $('#user-cropbox').Jcrop({  //options
+        onChange: update_crop,
+        onSelect: update_crop,
+        setSelect: [0, 0, 172, 172],  //设置一个初选框的位置
+        aspectRatio: 1,  //设置选择框高宽比,默认为任意
+        bgColor: 'white'
+        }, function(){
+        jcrop_api = this;
+      });
+    });
+  };
 
   function readData(evt) {
     var file = evt.target.files[0];
@@ -32,26 +51,8 @@ $(function() {
     });
   }
 
-  function init_jcrop() {
-    $(document).bind('cbox_complete', function() {
-      $('#user-cropbox').Jcrop({  //options
-        onChange: update_crop,
-        onSelect: update_crop,
-        setSelect: [0, 0, 172, 172],  //设置一个初选框的位置
-        aspectRatio: 1,  //设置选择框高宽比,默认为任意
-        bgColor: 'white'
-        }, function(){
-        jcrop_api = this;
-      });
-    });
-  };
-
-  function update_crop(coords) {
-    $("#user_crop_x").val(Math.round(coords.x));
-    $("#user_crop_y").val(Math.round(coords.y));
-    $("#user_crop_w").val(Math.round(coords.w));
-    $("#user_crop_h").val(Math.round(coords.h));
-  }
+  inputFile.addEventListener('click', function() { this.value = null; }, false);
+  inputFile.addEventListener('change', readData, false);
 
   $('.modal-footer form').submit(function() {
     $("#user-crop-btn").prop('disabled', true);
