c7960ce95c6951eb016772949b90da378409525f
Billie Zhang <billiecoder@gmail.com>
2015-02-14 15:41:49 +0800

    generate download token

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,7 +6,11 @@ class PhotosController < ApplicationController
   def create
     @photo = Photo.new
     @photo.name = params[:fname]
-    @photo.photo_url = params[:key]
+    primitive_url = 'http://7u2lgh.com1.z0.glb.clouddn.com/' + params[:key]
+    # generate a temporary download url for accessing resources saved in a private bucket
+    # @download_url = Qiniu::Auth.authorize_download_url(primitive_url)
+    # @photo.photo_url = download_url
+    @photo.photo_url = primitive_url
     @photo.save
   end
 end
